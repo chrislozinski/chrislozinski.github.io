a461135e3744c2da99cb95a40fcd2a73cfd0282a
Christopher Lozinski <clozinsk@uoguelph.ca>
2025-10-19 02:52:10 -0400

    more style fixes ugh help

--- js/navBar.js
+++ js/navBar.js
@@ -2,6 +2,7 @@
 
 // to load the navigaiton bar on each of the pages, this function will be called 
 // this was originally in my html file but gotta implement good oop practices lol
+/*
 function loadNavBar() {
     fetch('header.html')
         .then(response => response.text())
@@ -14,25 +15,15 @@ function loadNavBar() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar').innerHTML = data;
-            document.body.classList.add('loaded');
         }
     );
 }
 
-// to open the nav bar  
-/*
-function openNavBar() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-// to close the nav bar  
-function closeNavBar() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-*/
-
 document.addEventListener('DOMContentLoaded', function() {
     loadNavBar();
+});
+*/
+$(document).ready(function(){
+    $("#header").load("/header.html"); 
+    $("#navbar").load("/navMenu.html"); 
 });
