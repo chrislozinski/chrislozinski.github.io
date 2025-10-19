a241ff7cf2665e392611c96fcd4aeaa45c62d830
Christopher Lozinski <clozinsk@uoguelph.ca>
2025-10-19 02:27:19 -0400

    should fix page flashing

--- js/navBar.js
+++ js/navBar.js
@@ -14,6 +14,7 @@ function loadNavBar() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar').innerHTML = data;
+            document.body.classList.add('loaded');
         }
     );
 }
