d3872ffdc7627e22d94986cc2ef06c24ef2c79a7
Christopher Lozinski <clozinsk@uoguelph.ca>
2025-11-16 15:02:52 -0500

    trying to fix flashing along with some cleaner fonts

--- js/navBar.js
+++ js/navBar.js
@@ -3,22 +3,27 @@
 // to load the navigaiton bar on each of the pages, this function will be called 
 // this was originally in my html file but gotta implement good oop practices lol
 
-function loadNavBar() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header').innerHTML = data;
-        }
-    );
+async function loadNavBar() {
+    document.body.style.opacity = '0'; // ill try to hide body (dexter lol) at first to prevent flashing
     
-    fetch('navMenu.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        }
-    );
+    // load both components in parallel
+    const [headerHTML, navHTML] = await Promise.all([
+        fetch('../templates/header.html').then(r => r.text()),
+        fetch('../templates/navMenu.html').then(r => r.text())
+    ]);
+    
+    // insert content
+    document.getElementById('header').innerHTML = headerHTML;
+    document.getElementById('navbar').innerHTML = navHTML;
+    
+    // try to show body with fade in
+    document.body.style.opacity = '1';
+    document.body.style.transition = 'opacity 0.3s ease';
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+// load when DOM is ready
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', loadNavBar);
+} else {
     loadNavBar();
-});
+}
